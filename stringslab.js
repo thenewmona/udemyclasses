54a32c0646fecd0961783aafb69570273bacbf57
Ramona Saintandre <thenewmona@gmail.com>
2018-06-19 17:49:36 -0400

    added strings lab

---
+++ stringslab.js
@@ -0,0 +1,12 @@
+//strings used to represent text
+//trun any primitive data type into a string by adding single/double quotes
+//escape backslash to handle special characters 
+//lenght property and toUpperCase() method
+// change the following code to make valid strings
+
+var animal = 'hrose';
+var answer = 'false';
+
+var greeting1 = 'Sam says, "hi."';
+var greeting2 = "It's great to see you!";
+
