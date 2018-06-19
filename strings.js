2b0ed9fab8829da18affe1cd82508e4cb433ee8f
Ramona Saintandre <thenewmona@gmail.com>
2018-06-19 17:46:02 -0400

    first commit

---
+++ strings.js
@@ -0,0 +1,36 @@
+/* strings are a data type used to represent text and are wrapped in either a single/double quote.
+primitive data types 
+string, number,boolean,null,undefined,symbol
+
+Escape Character 
+The backslachescape character turns special characters into string characters
+\0 the NULL character
+\\backslash
+\n new line
+\r carriage return
+\v vertical tab
+\t tab
+\b bacspace 
+\f form feed 
+
+examples:
+var greeting ='It\'s great to see you!';
+var response = "Billy said, \"i am sick|"";
+
+LengthProperty = The length property returns the length of a string
+Way to know that something is a method and not a property is by the ()
+
+String method = 
+toUpperCase()
+ex: 
+*/
+
+var = name "Ramona";
+
+var greeting = 'Good to see you!';
+greeting; //'Good to see you!;
+
+
+var shoutGreeting =greeting.toUpperCase();
+shoutGreeting; //'GOOD TO SEE YOU!'
+
---
+++ variableslab.js
@@ -0,0 +1,16 @@
+//challenge//
+
+var 1stPet = 'Bear;
+var BaseballTeam = 'Dodgers';
+var favoritedayoftheweek = 'Friday';
+var var = 'car';
+
+var pet1 = 'Bear';
+
+var baseBallTeam = 'Dodgers';
+
+var favoriteDayOfTheWeek = 'Friday';
+
+var vechicle = 'car';
+
+var favorite_color = 'green';
